d7716f5bd85bc6a3d6d4b0e38fe02227c99b9a1a
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2018-10-12 09:52:26 +0100

    linkingtest

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<title>Jordan PC Austin</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="assets/css/base.css" />
+		<link rel="stylesheet" href="assets/css/base.css"/>
 
 	</head>
 	<body id="top">
