6111f601c476b47f3b1d181b52925e3f10cb5168
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-01-21 13:06:52 +0000

    ???????

--- index.php
+++ index.php
@@ -81,9 +81,10 @@
        <!--Navbar end-->
        <!--First Paragraph Begin-->
        <main>
+
        <div class="carousel carousel-slider">
-    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"></a>
-    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"></a>
+    <a class="carousel-item" href="#one!"><img src=""></a>
+    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2">nani</a>
     <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"></a>
     <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"></a>
   </div>
