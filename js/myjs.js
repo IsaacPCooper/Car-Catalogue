95fa852490c4cc599f2caabbf6333c812be12f45
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-01-21 13:13:50 +0000

    js canges

--- js/myjs.js
+++ js/myjs.js
@@ -9,11 +9,20 @@ $('.dropdown-button').dropdown({
      stopPropagation: false // Stops event propagation
    }
  );
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
 
   $(".button-collapse").sideNav();
 
 
-    var instance = M.Carousel.init({
-      fullWidth: true
-    });
+  $(document).ready(function(){
+   $('.carousel').carousel();
+
+   $('.carousel').carousel();
+   setInterval(function() {
+     $('.carousel').carousel('next');
+   }, 4500);
+});
+
+$('.carousel.carousel-slider').carousel({
+   fullWidth: true,
+   autoplay: true
+ });
