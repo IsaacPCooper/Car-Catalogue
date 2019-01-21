207ee0fa3a63f1fe09dfec1c6d8b03af6abba23a
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-01-21 13:44:37 +0000

    js fixes

--- js/myjs.js
+++ js/myjs.js
@@ -9,9 +9,11 @@ $('.dropdown-button').dropdown({
      stopPropagation: false // Stops event propagation
    }
  );
+ $( document ).ready(function(){
 
-  $(".button-collapse").sideNav();
+                 $(".button-collapse").sideNav();//mobile screen menu init
 
-  $('.carousel.carousel-slider').carousel({
-       fullWidth: true
-     });
+                 $('.carousel').carousel(); //carousel init
+
+                $('.carousel-slider').slider({full_width: true});//slider init
+             });
