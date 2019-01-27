e80a02358a226d751624a3d4aab130b576ef7fb9
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-01-27 18:01:05 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -8,9 +8,10 @@
       <h1>Signup</h1>
       <form action ="includes/signup.inc.php" method="post">
         <input type="text" name="uid" placeholder="Username">
-        <input type="text" name="mail" placeholder="E-mail"> 
+        <input type="text" name="mail" placeholder="E-mail">
         <input type="password" name="pwd" placeholder="Password">
         <input type="password" name="pwd-repeat" placeholder="Repeat Password">
+        <button type="submit" name="signup-submit">Signup</button>
       </form>
     </section>
   </div>
