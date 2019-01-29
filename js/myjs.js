db0cb5b41adc45275efa908dffa9f80a702e7996
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-01-29 08:59:43 +0000

    #fff
    white placeholders??

--- js/myjs.js
+++ js/myjs.js
@@ -9,11 +9,3 @@ $('.dropdown-button').dropdown({
      stopPropagation: false // Stops event propagation
    }
  );
- $( document ).ready(function(){
-
-                 $(".button-collapse").sideNav();//mobile screen menu init
-
-                 $('.carousel').carousel(); //carousel init
-
-                $('.carousel-slider').slider({full_width: true});//slider init
-             });
