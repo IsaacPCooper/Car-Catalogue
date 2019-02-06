82759d4eafb53c81d9c134bc9ab4a80b98ab402d
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-06 10:52:55 +0000

    Remove unneeded script in index

--- index.php
+++ index.php
@@ -4,17 +4,7 @@
        <!--Navbar end-->
        <!--First Paragraph Begin-->
        <main>
-         <!--Body scripts-->
-         <script type="text/javascript" language="javascript">
-              $( document ).ready(function(){
-
-                  $(".button-collapse").sideNav();//mobile screen menu init
-
-                  $('.carousel').carousel(); //carousel init
-
-                  $('.carousel-slider').slider({full_width: true});//slider init
-              });
-          </script>
+         <body>
          <h1 align="center"> TEST HEADER </h1>
          <div class="container">
      <div class="carousel carousel-slider">
