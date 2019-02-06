c99155116255dc8a7e16b3c105dc03f4a25b798a
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-06 10:05:12 +0000

    Added more to signup

--- signup.php
+++ signup.php
@@ -7,7 +7,9 @@
   <div class="container">
       <h1 align="center">Signup</h1>
       <form action ="includes/signup.inc.php" method="post">
-        <input type="text" name="uid" placeholder="Username">
+        <input type="text" name="fname" placeholder="First Name">
+        <input type="text" name="sname" placeholder="Last Name">
+        <input type="date" name="fname" placeholder="Date Of Birth: DD/MM/YYYY">
         <input type="text" name="mail" placeholder="E-mail">
         <input type="password" name="pwd" placeholder="Password">
         <input type="password" name="pwd-repeat" placeholder="Repeat Password">
