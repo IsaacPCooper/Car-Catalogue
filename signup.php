82fd98060cee71e2fba9fe5dcd57e1e32c771826
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-06 10:44:51 +0000

    Row passwords

--- signup.php
+++ signup.php
@@ -8,14 +8,26 @@
       <h1 align="center">Signup</h1>
       <form action ="includes/signup.inc.php" method="post">
         <div class="row">
-        <div class="col s6"><input type="text" name="fname" placeholder="First Name"></div>
-        <div class="col s6"><input type="text" name="sname" placeholder="Last Name"></div>
+        <div class="col s6">
+          <input type="text" name="fname" placeholder="First Name">
+        </div>
+        <div class="col s6">
+          <input type="text" name="sname" placeholder="Last Name">
+        </div>
       </div>
-        <input type="text" name="fname" placeholder="Date Of Birth: DD/MM/YYYY">
+        <input type="text" name="DOB" placeholder="Date Of Birth: DD/MM/YYYY">
         <input type="text" name="mail" placeholder="E-mail">
+      <div class="row">
+      <div class="col s6">
         <input type="password" name="pwd" placeholder="Password">
+      </div>
+      <div class="col s6">
         <input type="password" name="pwd-repeat" placeholder="Repeat Password">
-        <div align="center"><button type="submit" name="signup-submit">Sign Up</button></div>
+      </div>
+    </div>
+        <div align="center">
+          <button type="submit" name="signup-submit">Sign Up</button>
+        </div>
       </form>
   </div>
 </body>
