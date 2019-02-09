6971376f9e086d2a0f24e6d91cb3b0d9b2c8810a
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-09 18:24:08 +0000

    Moves to includes

---
+++ includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = "eu-cdbr-west-02.cleardb.net";
+$dBUsername = "b34530846c940e";
+$dBPassword = "5d27a577fc0dd95";
+$dBName = "heroku_79520a90f820d21;"
+
+$conn = mysqli_connect($servername,$dBUsername,$dBPassword,$dBPassword,$dBName);
+
+if(!$conn){
+  die("Connection failed: ".mysqli_connect_error());
+}
