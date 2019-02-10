39f1aa121d59640a83bd61aec2a157bd696c14f8
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-10 18:12:29 +0000

    connection fix

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,7 +5,8 @@ $dBUsername = "b34530846c940e";
 $dBPassword = "5d27a577fc0dd95";
 $dBName = "heroku_79520a90f820d21";
 
-$conn = mysqli_connect($servername,$dBUsername,$dBPassword,$dBPassword,$dBName);
+$conn = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b34530846c940e", "5d27a577fc0dd95", "heroku_79520a90f820d21") or die("Connection Failed".
+mysqli_error($conn));
 
 if(!$conn){
   die("Connection failed: ".mysqli_connect_error());
