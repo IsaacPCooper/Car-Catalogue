0d2c17ac1b990349cc7da89f946d2cad9c98afb7
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-10 17:36:37 +0000

    connection syntax fix

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $servername = "eu-cdbr-west-02.cleardb.net";
 $dBUsername = "b34530846c940e";
 $dBPassword = "5d27a577fc0dd95";
-$dBName = "heroku_79520a90f820d21;"
+$dBName = "heroku_79520a90f820d21";
 
 $conn = mysqli_connect($servername,$dBUsername,$dBPassword,$dBPassword,$dBName);
 
