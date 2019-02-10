cf9c9c24d1056e93a3dffcb28670d51f2f7cc138
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-10 18:46:02 +0000

    Location to location

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -12,37 +12,37 @@ $UserPwdRepeat = $_POST['pwd-repeat'];
 
   if(empty($UserFName) || empty($UserSName) || empty($UserDOB) ||empty ($UserEmail) || empty($UserPwd) || empty($UserPwdRepeat))
   {
-  header("Location: ../signup.php?error=emptyfields&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB."&mail=".$UserEmail);
+  header("location: ../signup.php?error=emptyfields&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB."&mail=".$UserEmail);
   exit();
   }
 
   else if (!filter_var($UserEmail, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z]*$/",$UserFName) && !preg_match("/^[a-zA-Z]*$/",$UserSName))
   {
-  header("Location: ../signup.php?error=invalidemail&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
+  header("location: ../signup.php?error=invalidemail&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
   exit();
   }
   else if (!filter_var($UserEmail, FILTER_VALIDATE_EMAIL)) {
-    header("Location: ../signup.php?error=invalidemail&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
+    header("location: ../signup.php?error=invalidemail&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
     exit();
   }
 
   else if (!preg_match("/^[a-zA-Z]*$/",$UserFName)) {
-    header("Location: ../signup.php?error=invalidfname&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
+    header("location: ../signup.php?error=invalidfname&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
     exit();
   }
   else if (!preg_match("/^[a-zA-Z]*$/",$UserSName)) {
-    header("Location: ../signup.php?error=invalidsname&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
+    header("location: ../signup.php?error=invalidsname&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
     exit();
   }
   else if ($UserPwd !== $UserPwdRepeat) {
-    header("Location: ../signup.php?error=invalidsname&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
+    header("location: ../signup.php?error=invalidsname&fname=".$UserFName."&sname=".$UserSName."&DOB=".$UserDOB);
     exit();
   }
   else {
     $sql = "SELECT UserEmail FROM users WHERE UserEmail=?";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-      header("Location: ../signup.php?error=sqlerror");
+      header("location: ../signup.php?error=sqlerror");
       exit();
     }
     else {
@@ -52,21 +52,21 @@ $UserPwdRepeat = $_POST['pwd-repeat'];
       $resultCheck = mysqli_stmt_num_rows($stmt);
 
       if($resultCheck > 0){
-        header("Location: ../signup.php?error=emailtaken");
+        header("location: ../signup.php?error=emailtaken");
         exit();
       }
       else {
         $sql = "INSERT INTO users FROM users(UserFName,UserSName,UserDOB,UserEmail,UserPwd) VALUES(?,?,?,?,?)";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-          header("Location: ../signup.php?error=sqlerror");
+          header("location: ../signup.php?error=sqlerror");
           exit();
         }
         else {
           $hashedPwd = password_hash($UserPwd,PASSWORD_DEFAULT)
           mysqli_stmt_bind_param($stmt,"sss",$UserFName,$UserSName,$UserDOB,$UserEmail,$hashedPwd);
           mysqli_stmt_execute($stmt);
-          header("Location: ../signup.php?signup=sucess");
+          header("location: ../signup.php?signup=sucess");
           exit();
 
         }
@@ -77,5 +77,5 @@ $UserPwdRepeat = $_POST['pwd-repeat'];
   mysqli_close($conn);
 }
 else {
-  header("Location: ../signup.php");
+  header("location: ../signup.php");
 }
