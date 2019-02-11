0b2db458a40e6da9a48291dd3a208f8b3cf9880f
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-11 17:15:27 +0000

    added cars.php

---
+++ cars.php
@@ -0,0 +1,23 @@
+    <?php
+     require "includes/header.php";
+     ?>
+       <!--Navbar end-->
+       <!--First Paragraph Begin-->
+       <main>
+         <body>
+         <h1 align="center"> Top cars, Top Prices! </h1>
+         <div class="container">
+     <div class="carousel carousel-slider">
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+  </div>
+</div>
+</main>
+<!--Footer Start-->
+<?php
+require "includes/footer.php";
+ ?>
+</body>
+ </html>
