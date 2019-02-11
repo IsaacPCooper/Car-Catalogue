9129eb6aa684bc4b547f80c53d67c57d1d661155
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-11 17:18:33 +0000

    fill

--- aboutus.php
+++ aboutus.php
@@ -5,14 +5,8 @@
        <!--First Paragraph Begin-->
        <main>
          <body>
-         <h1 align="center"> Top cars, Top Prices! </h1>
+         <h1 align="center"> Learn about JPA cars</h1>
          <div class="container">
-     <div class="carousel carousel-slider">
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-  </div>
 </div>
 </main>
 <!--Footer Start-->
