6517cbe14033585ffc00654ec44d112328c0d8cc
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-11 17:11:17 +0000

    login page added

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+ require "includes/header.php";
+ ?>
+
+<main>
+  <!--header-->
+  </header>
+  <div align="center"class="header">
+    <h1>Log in</h1>
+  </div>
+
+<form method="post" action="includes/login.inc.php">
+  <!--First Name Input w/ Label-->
+  <div class="container">
+<div class="input-group">
+  <label>First Name</label>
+  <input type="text" name="UserFName">
+</div>
+  <!--Second Name Input w/ Label-->
+<div class="input-group">
+  <label>Second Name</label>
+  <input type="text" name="UserSName">
+</div>
+  <!--DOB Input w/ Label-->
+<div class="input-group">
+  <label>Date Of Birth</label>
+  <input type="text" name="UserDOB">
+</div>
+  <!--Email Input w/ Label-->
+<div class="input-group">
+  <label>Email</label>
+  <input type="text" name="UserEmail">
+</div>
+  <!--Password input w/ Label-->
+<div class="input-group">
+  <label>Password</label>
+  <input type="password" name="UserPwd">
+</div>
+  <!--Password repeat w/ Label-->
+<div class="input-group">
+  <label>Repeat Password</label>
+  <input type="password" name="UserPwd-repeat">
+</div>
+<div align="center" class="input-group">
+  <button type="submit"name="login-submit"class="btn">Log In</button>
+</div>
+</div>
+</form>
+</main>
+
+ <?php
+ require"includes/footer.php";
+  ?>
