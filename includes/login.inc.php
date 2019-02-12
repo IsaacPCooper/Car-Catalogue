dabda00072c58ebc1eb3e9a72d988b24c15b2085
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-12 15:12:06 +0000

    login page made

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,7 +1,36 @@
 <?php
+//Check if user clicked button
 if (isset($_POST['login-submit'])) {
+    require 'dbConnect.php';
+    $mailuid = $_POST['mailuid'];
+    $password = $_POST['pwd'];
+//empty field checks
+  if empty($mailuid) || empty($password))
+  {
+    header("Location:../login.php?error=emptyfields");
+    exit();
+  }
+  else {
+    $sql = "SELECT * FROM users WHERE UserEmail=?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+      header("Location:../login.php?error=sqlerror");
+      exit();
+    }
+    else {
 
+
+      mysqli_stmt_bind_param($stmt,"ss", $mailuid,$mailuid);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      if (condition) {
+        
+      }
+    }
+
+  }
 }
+//if not, kick them back to login.php
 else {
   header("Location: ../login.php");
   exit();
--- login.php
+++ login.php
@@ -10,36 +10,16 @@
   </div>
 
 <form method="post" action="includes/login.inc.php">
-  <!--First Name Input w/ Label-->
+  <!--Email w/ Label-->
   <div class="container">
-<div class="input-group">
-  <label>First Name</label>
-  <input type="text" name="UserFName">
-</div>
-  <!--Second Name Input w/ Label-->
-<div class="input-group">
-  <label>Second Name</label>
-  <input type="text" name="UserSName">
-</div>
-  <!--DOB Input w/ Label-->
-<div class="input-group">
-  <label>Date Of Birth</label>
-  <input type="text" name="UserDOB">
-</div>
-  <!--Email Input w/ Label-->
-<div class="input-group">
-  <label>Email</label>
-  <input type="text" name="UserEmail">
-</div>
+    <div class="input-group">
+      <label>Email</label>
+      <input type="text" name="mailuid">
+    </div>
   <!--Password input w/ Label-->
 <div class="input-group">
   <label>Password</label>
-  <input type="password" name="UserPwd">
-</div>
-  <!--Password repeat w/ Label-->
-<div class="input-group">
-  <label>Repeat Password</label>
-  <input type="password" name="UserPwd-repeat">
+  <input type="password" name="pwd">
 </div>
 <div align="center" class="input-group">
   <button type="submit"name="login-submit"class="btn">Log In</button>
