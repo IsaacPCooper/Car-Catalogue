709b04802a372514b53923f3cce3d42ea68809a0
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-18 22:54:30 +0000

    Update index.php
    shrink a lil

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
          <body>
          <h1 align="center"> Top cars, Top Prices! </h1>
          <div class="container">
-           <img class="responsive-img"src="assets/img/money.jpeg">
+           <img height="50%" width="50% "class="responsive-img"src="assets/img/money.jpeg">
     <div align="center">
       <?php
            if (isset($_SESSION['UserFName'])) {
