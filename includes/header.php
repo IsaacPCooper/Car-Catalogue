cc4f7f546802747fdf6136435a9e1b9b4f8f2cff
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-18 22:49:06 +0000

    Update header.php
    !!

--- includes/header.php
+++ includes/header.php
@@ -58,8 +58,16 @@ session_start();
           ?>
        </ul>
        <ul id="dropdown6" class="dropdown-content gfont1">
-         <li><a class="gfont1" href="signup.php">Register</a></li>
-         <li><a class="gfont1" href="login.php">Login</a></li>
+         <?php
+              if (isset($_SESSION['UserFName'])) {
+                echo '<li><a class="gfont1" href="#">Change Account</a></li>
+                      <li><a class="gfont1" href="logout.inc.php">Log Out</a></li>';
+              }
+              else {
+                echo '<li><a class="gfont1" href="signup.php">Register</a></li>
+         <li><a class="gfont1" href="login.php">Login</a></li>';
+        }
+          ?>
        </ul>
        <!-- Dropdown content end-->
        <!--Navbar Begin-->
