5d012d7c320a86c99ebc2877a8f8280b9e5b92d5
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-18 22:03:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,13 @@
          <body>
          <h1 align="center"> Top cars, Top Prices! </h1>
          <div class="container">
-           <?php
+     <div class="carousel carousel-slider">
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+    <div align="center">
+      <?php
            if (isset($_SESSION['UserFName'])) {
              echo '<p> You are Logged In! </p>';
            }
@@ -15,11 +21,7 @@
               echo '<p> You are Logged out! </p>';
            }
            ?>
-     <div class="carousel carousel-slider">
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
-    <a class="carousel-item" href="#"><img height="80%" width="80%" src="https://images.pexels.com/photos/1849115/pexels-photo-1849115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></a>
+</div>
   </div>
 </div>
 </main>
