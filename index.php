aeb11223e2aef9c5c90a0a11eb4564cf2071a92d
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-18 22:56:02 +0000

    Update index.php
    ??????

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
          <body>
          <h1 align="center"> Top cars, Top Prices! </h1>
          <div align="center" class="container">
-           <img height="50%" width="50% "class="responsive-img" src="assets/img/money.jpeg">
+           <img align ="center"height="50%" width="50% "class="responsive-img" src="assets/img/money.jpeg">
       <?php
            if (isset($_SESSION['UserFName'])) {
              echo '<p> You are Logged In! </p>';
