c5f9f1d8c3d067fca3f7d2d895f21e97b8a7947d
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-20 17:58:06 +0000

    added edit user

--- edituser.php
+++ edituser.php
@@ -6,40 +6,20 @@
   <!--header-->
   </header>
   <div align="center"class="header">
-    <h1>Sign Up!</h1>
+    <h1>Edit User Information</h1>
   </div>
 
 <form method="post" action="includes/signup.inc.php">
-  <!--First Name Input w/ Label-->
   <div class="container">
-<div class="input-group">
-  <label>First Name</label>
-  <input type="text" name="UserFName">
-</div>
-  <!--Second Name Input w/ Label-->
-<div class="input-group">
-  <label>Second Name</label>
-  <input type="text" name="UserSName">
-</div>
   <!--DOB Input w/ Label-->
 <div class="input-group">
   <label>Date Of Birth</label>
-  <input type="text" name="UserDOB">
-</div>
-  <!--Email Input w/ Label-->
-<div class="input-group">
-  <label>Email</label>
-  <input type="text" name="UserEmail">
-</div>
-  <!--Password input w/ Label-->
-<div class="input-group">
-  <label>Password</label>
-  <input type="password" name="UserPwd">
+  <input type="text" name="editDOB">
 </div>
   <!--Password repeat w/ Label-->
 <div class="input-group">
-  <label>Repeat Password</label>
-  <input type="password" name="UserPwd-repeat">
+  <label>Phone Number</label>
+  <input type="text" name="editPhone">
 </div>
 <div align="center" class="input-group">
   <button type="submit" name="edit-user" class="btn">Sign Up</button>
