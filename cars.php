fa755f01f2e166e537f6cafe4bf998e22963ecab
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-22 19:18:56 +0000

    Update cars.php

--- cars.php
+++ cars.php
@@ -8,7 +8,7 @@
          <h1 align="center">View our products</h1>
          <form action="search.php" method="post">
            <input type="text" name="search" placeholder="Search">
-           <button type="submit" name="submit-search">M/button>
+           <button type="submit" name="submit-search"></button>
          <div class="container">
      <div class="carousel carousel-slider">
     <a class="carousel-item responsive-img"><img src="assets/img/car_1.png"></a>
