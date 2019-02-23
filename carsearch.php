68f8c163137626df396a61cd9e0d1d4b92a3908c
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-23 21:15:52 +0000

    Update carsearch.php

--- carsearch.php
+++ carsearch.php
@@ -26,8 +26,11 @@ $keyword = $_POST['search'];
 
 if (isset($_SESSION['UserID'])){
   $sql3 = "SELECT Searched, count(User_ID) FROM searches WHERE User_ID = $UserID GROUP BY count(User_ID) LIMIT 3";
-  $stmt3 = mysqli_stmt_init($conn,$sql3);
-  echo "<h3> Favourite searches</h3> </br>", "<p>".$stmt3."</p>";
+  $result3 = mysqli_query($conn ,$sql3);
+  $queryResult3 = mysqli_num_rows($result3);
+  if ($queryResult3 > 0) {
+    while($row = mysqli_fetch_assoc($result3)) {
+  echo "<h3> Favourite searches</h3> </br>", "<p>".$row['Searched']."</p>";
   }
 
 
