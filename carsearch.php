46cc44ca4d29439feed740340263281575159f16
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-23 17:51:23 +0000

    Update carsearch.php

--- carsearch.php
+++ carsearch.php
@@ -24,8 +24,8 @@ if (isset($_POST['submit-search'])){
   $search = mysqli_real_escape_string($conn,$_POST['search']);
   $sql = "SELECT * FROM cars WHERE carName LIKE '%$search%'OR carMake LIKE '%$search%'OR carYear LIKE '%$search%'OR carType LIKE '%$search%'OR carSize LIKE '%$search%' OR carFuel LIKE '%$search%'";
   $sql2 = "INSERT INTO users(UserPSearches) VALUES(['search'])";
-  $_SESSION['PSearch'] = ['search']
-  $result = mysqli_query($conn,$sql);
+  $_SESSION['PSearch'] = ['search'];
+  $result = mysqli_query($conn ,$sql);
   $queryResult = mysqli_num_rows($result);
 
 if ($queryResult > 0) {
@@ -47,7 +47,7 @@ else {
   }
 }
 if (isset($_POST['favourite-search'])){
-  $sql3 = "INSERT INTO users(UserFSearch) VALUES($_SESSION['PSearch'])";
+  $sql3 = "INSERT INTO users(UserFSearch) VALUES(['PSearch'])";
 }
  ?>
 </div>
