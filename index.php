40310cef4db1b7020d3434963fb010f05ee75549
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-24 00:58:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
            <img align ="center" width="50% "height="50%" src="assets/img/money.jpeg">
       <?php
            if (isset($_SESSION['UserID'])) {
+             $sql = "SELECT User_ID, Searched FROM searches HAVING COUNT(*) > 2";
              echo "<h3> You are Logged In! </h3>";
              echo "<h4> Welcome, ".$_SESSION['UserFName']." !</h4>";
+             echo "<p> Your favourite search items are:",$sql;
            }
            else {
              echo "<h3> Please Log In to Continue </h3>";
