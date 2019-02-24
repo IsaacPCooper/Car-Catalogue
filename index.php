60416de1eed875c9e2f53f8d46efa2b20c2660ef
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-24 01:07:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,6 @@
              echo "<h3> You are Logged In! </h3>";
              echo "<h4> Welcome, ".$_SESSION['UserFName']." !</h4>";
              echo "<p> Your favourite search items are:",mysqli_result($result,0),"</p>";
-             echo "<p>",mysqli_result($result,1),"</p>";
-             echo "<p>",mysqli_result($result,2),"</p>";
            }
            else {
              echo "<h3> Please Log In to Continue </h3>";
