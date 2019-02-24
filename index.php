bad11909ea61ca8261e7219b8de90d3035c99662
Jordan Philip Austin <Jordan.P.C.Austin@gmail.com>
2019-02-24 20:16:16 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
     <?php
      require "includes/header.php";
-     $id = $_SESSION['UserID'];
      ?>
        <!--Navbar end-->
        <!--First Paragraph Begin-->
@@ -11,27 +10,12 @@
            <img align ="center" width="50% "height="50%" src="assets/img/money.jpeg">
       <?php
            if (isset($_SESSION['UserID'])) {
-             $fav= "SELECT * FROM searches WHERE User_ID = $id GROUP BY Searched ORDER by COUNT(*) Desc limit 1";
-             $result = mysqli_query($conn, $fav);
-             $datas = array();
-           if ($result):
-             if (mysqli_num_rows($result) > 0):
-              while ($row = mysqli_fetch_assoc($result)):
-                $datas[] = $row;
-                foreach ($datas as $data){
-                  echo $data['Searched'];
-                  echo"<br>";
-                  echo "<h3> You are Logged In! </h3>";
-                  echo "<h4> Welcome, ".$_SESSION['UserFName']." !</h4>";
+             echo "<h3> You are Logged In! </h3>";
+             echo "<h4> Welcome, ".$_SESSION['UserFName']." !</h4>";
            }
-          // else {
-             //echo "<h3> Please Log In to Continue </h3>";
-          // }
-
-endwhile;
-endif;
-endif;
-}
+           else {
+             echo "<h3> Please Log In to Continue </h3>";
+           }
            ?>
 </div>
 </main>
